0d10c91021faf68c4a9303c55808cf467e3a2ec9
Edsel Caprice <caprice@mail.usf.edu>
2018-05-19 02:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function updateObjectWithKeyAndValue(obj,key,value) {
   return Object.assign({},obj,{[key]:value});
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {obj[key]=value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj,key) {
+  
+}
